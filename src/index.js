fb9bd0d1a576420158431bd6bab77fc33d36ea24
Alex <ovcharenko@octarine.com.ua>
2020-10-01 18:06:17 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) {
+    return 0;
+  } else {
+    let i = array.length, 
+    min = Infinity;
+    while (i--) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) {
+    return 0;
+  } else {
+    let i = array.length, 
+    max = -Infinity;
+    while (i--) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) {
+    return 0;
+  } else {
+    let sum = 0;
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+      result = sum / (array.length);
+    }
+    return result;
+  }
 }
